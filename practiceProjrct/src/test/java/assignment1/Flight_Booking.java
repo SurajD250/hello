9ddd2707189d20677219de5f9861d1d8ccb04972
SurajD250 <suraj.dalavi811@gmail.com>
2023-06-05 17:21:34 +0530

    fourth commit

--- practiceProjrct/src/test/java/assignment1/Flight_Booking.java
+++ practiceProjrct/src/test/java/assignment1/Flight_Booking.java
@@ -15,7 +15,7 @@ public class Flight_Booking {
 	@Test
 	void book_Spicejet_com() throws InterruptedException {
 		
-		System.out.println("changes made");
+		System.out.println("changes ma");
 		
 	}
 
