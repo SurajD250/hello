c10719a87fa8fc70ca4e5bf422f8e25c3a2baa03
SurajD250 <s43459162@gmail.com>
2023-06-05 17:06:45 +0530

    second commit

--- practiceProjrct/src/test/java/assignment1/Flight_Booking.java
+++ practiceProjrct/src/test/java/assignment1/Flight_Booking.java
@@ -15,36 +15,7 @@ public class Flight_Booking {
 	@Test
 	void book_Spicejet_com() throws InterruptedException {
 		
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
-		driver.get("https://book.spicejet.com/");
-		driver.findElement(By.xpath("//span[text()=' Flights ']")).click();
-		driver.findElement(By.id("ControlGroupSearchView_AvailabilitySearchInputSearchView_RoundTrip")).click();
-		driver.findElement(By.id("ControlGroupSearchView_AvailabilitySearchInputSearchVieworiginStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[text()=' Mumbai (BOM)']")).click();
-		driver.findElement(By.id("ControlGroupSearchView_AvailabilitySearchInputSearchViewdestinationStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[text()=' Delhi (DEL)']")).click();
-		driver.findElement(By.xpath("//a[@class='ui-state-default' and @data-date='28']")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("//input[@id='date_picker_id_2']/following-sibling::button[@type='button']")).click();
-		driver.findElement(By.xpath("//a[@class='ui-state-default' and @data-date='30']")).click();
-		WebElement dp = driver.findElement(By.id("divpaxinfo"));
-		dp.click();
-		WebElement adult = driver.findElement(By.name("ControlGroupSearchView$AvailabilitySearchInputSearchView$DropDownListPassengerType_ADT"));
-		Select sl=new Select(adult);
-		sl.selectByValue("3");
-		Thread.sleep(2000);
-		dp.click();
-		dp.click();
-		WebElement child=driver.findElement(By.xpath("//select[@name='ControlGroupSearchView$AvailabilitySearchInputSearchView$DropDownListPassengerType_CHD']"));
-		child.click();
-		Select sl2=new Select(child);
-		sl.selectByValue("2");
-		driver.findElement(By.id("ControlGroupSearchView_AvailabilitySearchInputSearchView_ButtonSubmit")).click();
-		WebElement DEPARTURE_FLIGHT = driver.findElement(By.xpath("//table[@id='availabilityTable0']/descendant::span[@class='bound-city uppercase']"));
-		Assert.assertEquals(DEPARTURE_FLIGHT.isDisplayed(), true);
+		System.out.println("hello");
 		
 	}
 
